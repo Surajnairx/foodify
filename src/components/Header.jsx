eaf99148273bf14c153aca7252dfbb68cd5fb482
Suraj Nair <surajnair9698@gmail.com>
2025-10-28 23:44:58 +0530

    feat: implement checkout function -stage 1: display total and form for user details

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,15 @@ import { useContext } from "react";
 import logo from "../assets/logo.jpg";
 import Button from "../UI/Button";
 import CartContext from "../context/CartContext";
+import UserProgressContext from "../context/UserProgressContext";
 
 function Header() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
   const totalCartItems = cartCtx.items.reduce((total, item) => {
     return (total += item.quantity);
   }, 0);
@@ -15,7 +21,9 @@ function Header() {
         <h1>Foodify</h1>
       </div>
       <nav>
-        <Button textOnly>Cart({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
